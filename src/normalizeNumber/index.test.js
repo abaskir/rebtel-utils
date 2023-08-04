a7a14ef5477a3c53ff6e091f85993f120e88f00d
abaskir <alper.baskir@rebtel.com>
2023-08-04 14:02:31 +0200

    unit test for normalizeNumber added

--- src/normalizeNumber/index.test.js
+++ src/normalizeNumber/index.test.js
@@ -1,9 +1,25 @@
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const {normalizeNumber } = require('../../../dist/lib/es5');
-
+const {normalizeNumber } = require('../../dist/lib/es5');
+jest.mock('../../dist/lib/es5', () => ({
+    normalizeNumber : jest.fn(),
+}));
 describe('normalize number', ()=>{
-    test('basic', ()=>{
-        expect(normalizeNumber()).toBe(false);
+    test('making request',async ()=>{
+        normalizeNumber.mockImplementation(() => {
+            return { response: {
+                data:{
+                    countryCode : "52",
+                    countryId: "MX",
+                    endpoint: "+523334858347",
+                    formatted: "+52 33 3485 8347",
+                    numberType: "FixedLine",
+                    numberTypeResource: "Landline",
+                    type: "number",
+                  },
+            },
+            };
+          });
+          expect(normalizeNumber("+523334858347", "MX")).toEqual( {"response": {"data": {"countryCode": "52", "countryId": "MX", "endpoint": "+523334858347", "formatted": "+52 33 3485 8347", "numberType": "FixedLine", "numberTypeResource": "Landline", "type": "number"}}});
     });
 });
